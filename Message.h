212740831df8e738551cf0bfe487531a9dd45e62
Henman <113828858+leo37rus@users.noreply.github.com>
2022-11-25 20:51:06 +0300

    Update Message.h

--- Message.h
+++ Message.h
@@ -1,8 +1,18 @@
-﻿#pragma once
+#pragma once
 #include "User.h"
 
 class Message
 {
-	
+	const std::string _from;//откуда
+	const std::string _to;// кому
+	const std::string _text;//текст
+
+public:
+	Message(const std::string& from, const std::string& to, const std::string& text)
+		:_from(from), _to(to), _text(text) {}
+
+	const std::string& getFrom() { return _from; }
+	const std::string& getTo() { return _to; }
+	const std::string& getText() { return _text; }
 
 };
