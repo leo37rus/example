3ceb614d83f0dc5682a3b61f8d1abfa5d7c50876
belujimi <belujimi@belujimi.ru>
2022-11-26 02:18:38 +0500

    ещё исправления названий функций к единому стилю

--- Message.h
+++ Message.h
@@ -11,8 +11,8 @@ public:
 	Message(const std::string& from, const std::string& to, const std::string& text)
 		:_from(from), _to(to), _text(text) {}
 
-	const std::string& getFrom() { return _from; }
-	const std::string& getTo() { return _to; }
-	const std::string& getText() { return _text; }
+	const std::string& get_From() { return _from; }
+	const std::string& get_To() { return _to; }
+	const std::string& get_Text() { return _text; }
 
 };
