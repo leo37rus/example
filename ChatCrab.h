13e404eedc73ac2663b703d8031f6b09a3b345a7
Henman <113828858+leo37rus@users.noreply.github.com>
2022-11-25 20:52:16 +0300

    Update ChatCrab.h

--- ChatCrab.h
+++ ChatCrab.h
@@ -1,13 +1,35 @@
-﻿#pragma once
+#pragma once
 #include<vector>
 #include<exception>
 #include<memory>
 #include"Message.h"
 
 
-
 class Chat
 {
+	bool WorkChat_ = false; //WorkChat_ isChatWork_
+	std::vector<User> AllUsers_;// users_ AllUsers_
+	std::vector<Message> messages_;
+	std::shared_ptr<User> currentUser_ = nullptr; //текущий пользователь
+
+	void registration_in_the_chat();//регестрация в чате
+	void log_ln_to_the_chat();// вход в чат
 	
+	void show_chat() const;//показать чат
+
+	void show_all_users_name() const;// показать всех пользователей
+
+	void add_message();//добавить сообщение
+
+	std::vector<User>& get_all_users() { return AllUsers_; }//получить список пользователей
+	std::vector<Message>& get_all_messages() { return messages_; }// получить все сообщения
+	std::shared_ptr<User> get_user_by_login(const std::string& login) const;//getAUserLogin
+	std::shared_ptr<User> get_user_by_name(const std::string& name) const;//getAUserName
+public:
+	void start() {WorkChat_ = true;}
+	bool isChatWork() const { return WorkChat_; }
+	std::shared_ptr<User> getCurrentUser() const { return currentUser_; }
+	void show_login_menu();
+	void showUserMenu();
 
 };
