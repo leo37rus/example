8636c27f9ee603fe9ec899d2d6e92141d1a16a24
Henman <113828858+leo37rus@users.noreply.github.com>
2022-11-25 20:50:42 +0300

    Update User.h

--- User.h
+++ User.h
@@ -1,6 +1,18 @@
-﻿#pragma once
+#pragma once
 #include <string>
 class User
 {
-	
+	std::string login_;
+	std::string password_;
+	std::string name_;
+
+public:
+	User( const std::string &login, const std::string& password, const std::string& name)
+		:login_(login), password_(password), name_(name) {}
+
+	const std::string& get_user_login() { return login_; }
+	const std::string& getUserPassword() { return password_; }
+	void setUserPassword(const std::string& password) { password_ = password; }
+	const std::string& getUserName() { return name_; }
+	void setUserName(const std::string& name) { name_ = name; }
 };
