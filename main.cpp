649d35472a34a412514b3ef87b631fedbaa8881a
Henman <113828858+leo37rus@users.noreply.github.com>
2022-11-26 12:09:38 +0300

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,12 +1,21 @@
-﻿#include <iostream>
+#include <iostream>
 #include "ChatCrab.h"
 
 
-int main() {
-	setlocale(LC_ALL, "ru");
-	Chat chat;
-	chat.start(); // startAChat
-	chat.show_login_menu();
+int main() 
+{
+    setlocale(LC_ALL, "ru");// мой xcode не знает что это такое
+    Chat chat;
+    chat.start();
 
-	return 0;
+    while (chat.workChat())
+    {
+        chat.show_Log_Menu();
+
+        while (chat.get_Current_User())
+        {
+            chat.show_User_Menu();
+        }
+    }
+    return 0;
 }
