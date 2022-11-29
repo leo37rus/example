fc47400a33531bbe3e24cb1b2beeb16a2fefc8d8
Henman <113828858+leo37rus@users.noreply.github.com>
2022-11-29 20:16:59 +0300

    Update Message.h

--- Message.h
+++ Message.h
@@ -3,16 +3,16 @@
 
 class Message
 {
-	const std::string _from;//откуда
-	const std::string _to;// кому
-	const std::string _text;//текст
+	const std::string _from;
+	const std::string _to;
+	const std::string _text;
 
 public:
 	Message(const std::string& from, const std::string& to, const std::string& text)
 		:_from(from), _to(to), _text(text) {}
 
-	const std::string& getFrom() { return _from; }
-	const std::string& getTo() { return _to; }
-	const std::string& getText() { return _text; }
+	const std::string& getFrom() const { return _from; }
+	const std::string& getTo() const { return _to; }
+	const std::string& getText() const { return _text; }
 
 };
